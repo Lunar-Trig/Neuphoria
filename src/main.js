dce35d44c2df35f2996d0ed8c69660983a9c00e6
Stacey <cocoash91@gmail.com>
2023-12-18 20:54:50 -0500

    made some changes on connect page

--- src/main.js
+++ src/main.js
@@ -8,3 +8,24 @@ function openSlideMenu(){
   function closeSlideMenu(){
     document.getElementById('side-menu').style.width = '0px';
   }
+
+// for copying text 
+function copyEmail(){
+  const text = document.getElementById('email');
+  const storage = document.createElement('textarea')
+  storage.value = text.innerHTML;
+  text.appendChild(storage);
+  storage.select();
+  document.execCommand('copy');
+  text.removeChild(storage);
+}
+
+function copyNumber(){
+  const text = document.getElementById('num');
+  const storage = document.createElement('textarea')
+  storage.value = text.innerHTML;
+  text.appendChild(storage);
+  storage.select();
+  document.execCommand('copy');
+  text.removeChild(storage);
+}
